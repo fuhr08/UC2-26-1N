dcb973a74a84d0cdef1bdea0a8006c7075f1cdf7
fuhr08 <gabrielfuhr08@gmail.com>
2026-03-26 21:37:18 -0300

    Add files via upload

---
+++ 25-03.js
@@ -0,0 +1,93 @@
+/*exercicio1
+//1
+let firstValue = 3
+let secondValue = 4
+let result = firstValue + secondValue
+console.log(result)
+
+//2
+let three = 3
+let five = 5
+let product = ((three * five) / 2)
+console.log(product)
+
+//3
+let value4 = 4
+let value5 = 5
+let answer = value4 - value5
+let finalAnswer = answer * -1
+console.log(finalAnswer)
+
+//4
+let mod = 234 % 5
+console.log(mod)
+
+let v1 = 7
+let v2 = 9
+console.log("O primeiro numero e igual ao segundo?", v1 === v2)
+console.log("O primeiro numero e diferente do segundo?", v1 !== v2) 
+console.log("O primeiro numero e maior que o segundo?", v1 > v2)
+console.log("O primeiro numero e menor que o segundo?", v1 <v2)
+
+let a = true 
+let b = false
+let c = true
+
+console.log(a && b)
+console.log(b && c)
+console.log(a && c)
+console.log(a && b && c)
+
+let a = true
+let b = false
+let c = true
+
+let result1 = a||b
+console.log("a||b", result1)
+
+let result2 = b||c
+console.log("b||c", result2)
+
+let result3 = a||c
+console.log("a||c", result3)
+
+let result4 = a||b||c
+console.log("a||b||c", result4)
+
+const nome = "Gabriel"
+console.log("Nome:", nome)
+let age = 18
+console.log("Idade:", age)
+console.log("É maior de idade?", age >= 18)
+console.log("Idade em 2050:", age + 24)
+
+const nome = "Gabriel"
+console.log("Nome:", nome)
+let age = 18
+console.log("Idade:", age)
+console.log("É maior de idade:", age >= 18)
+console.log("Idade em 2050:", age + 24)
+
+const nome = "Gabriel"
+console.log("Nome:",nome)
+let age = 18
+console.log("Idade:",age)
+console.log("É maior de idade?",age >= 18)
+console.log("Idade em 2050:",age + 24)
+
+const firstName = "Gabriel"
+console.log("Nome:",firstName)
+let age = 18
+console.log("Idade:", age)
+console.log("É maior de idade?", age >= 18)
+console.log("Iade em 2050:",age+24)*/
+
+let nome = "Gabriel"
+let anodenascimento = 2008
+let anoatual = 2026
+
+console.log("Nome:", nome)
+console.log("Idade:", anoatual - anodenascimento)
+console.log("É maior de idade?", anoatual - anodenascimento >= 18)
+console.log("Em 2050", nome, "tera", 2050 - anodenascimento, "idade")
+
